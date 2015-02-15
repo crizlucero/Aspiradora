8400b816aab2216c1dc30fc4b42500acda0ddc22
Christian Lucero <crizlucero@gmail.com>
2015-02-15 17:14:28 -0600

    Proyecto del agente "Aspiradora"

---
+++ Aspiradora/Agente.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Aspiradora
+{
+    class Agente
+    {
+        private string Nombre;
+        private Grid edoInicial { get; set; }
+        private Grid edoActual { get; set; }
+        private int[] posicion = new int[2];
+        public Agente(Grid Entorno)
+        {
+            this.Nombre = "Aspiradora";
+            this.edoInicial = Entorno;
+            this.edoActual = Entorno;
+            this.posicion[0] = Grid.GetColumn(Entorno.Children[0]);
+            this.posicion[1] = Grid.GetRow(Entorno.Children[0]);
+        }
+
+        public void Reglas()
+        {
+            //Girar derecha, moverse una posicion para enfrente y girar de nuevo
+            if ((this.posicion[1] % 2 == 0 && this.posicion[0] == 7) || (this.posicion[1] % 2 != 0 && this.posicion[0] == 0))
+            {
+                this.posicion[1]++;
+                Grid.SetRow(this.edoActual.Children[0], this.posicion[1]);
+            }
+            else if (this.posicion[0] == 7 && this.posicion[1] == 0) //posición final, pasar al agente a posición inicial
+            {
+                this.posicion[0] = 0;
+                this.posicion[1] = 0;
+            }
+        }
+
+        public void MoverAgente()
+        {
+            //Limpiar basura
+            foreach (var espacio in this.edoActual.Children)
+            {
+                if (espacio is Rectangle)
+                    if (((Rectangle)espacio).Fill.ToString() == Colors.Brown.ToString())
+                        this.LimpiarPiso(espacio);
+            }
+            if (this.posicion[0] != 0 || this.posicion[1] != 7)
+            {
+                Console.WriteLine("Posicion: " + this.posicion[0] + this.posicion[1]);
+
+                if (this.posicion[1] % 2 == 0)
+                {
+                    if (this.posicion[0] != 7)
+                    {
+                        //Mover derecha
+                        this.posicion[0]++;
+                        Grid.SetColumn(this.edoActual.Children[0], this.posicion[0]);
+                    }
+                    else
+                    {
+                        this.posicion[1]++;
+                        Grid.SetRow(this.edoActual.Children[0], this.posicion[1]);
+                        ((Label)this.edoActual.Children[0]).Content = "<";
+                    }
+                }
+                else if (this.posicion[1] % 2 != 0)
+                {
+                    if (this.posicion[0] != 0)
+                    {
+                        //Mover izquierda
+                        this.posicion[0]--;
+                        Grid.SetColumn(this.edoActual.Children[0], this.posicion[0]);
+                    }
+                    else
+                    {
+                        this.posicion[1]++;
+                        Grid.SetRow(this.edoActual.Children[0], this.posicion[1]);
+                        ((Label)this.edoActual.Children[0]).Content = ">";
+                    }
+                }
+            }
+            else
+            {
+                this.posicion[0] = 0;
+                this.posicion[1] = 0;
+                Grid.SetColumn(this.edoActual.Children[0], this.posicion[0]);
+                Grid.SetRow(this.edoActual.Children[0], this.posicion[1]);
+                ((Label)this.edoActual.Children[0]).Content = ">";
+            }
+        }
+        private void LimpiarPiso(object espacio)
+        {
+            if (Grid.GetColumn((UIElement)espacio) == this.posicion[0] && Grid.GetRow((UIElement)espacio) == this.posicion[1])
+                ((Rectangle)espacio).Fill = new SolidColorBrush(Colors.White);
+        }
+    }
+}
---
+++ Aspiradora/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Aspiradora
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Agente aspiradora;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.ColocarBasura();
+            this.InitAgente();
+        }
+
+        private void ColocarBasura()
+        {
+            Random rand = new Random();
+            int total = rand.Next(1, 64);
+            int x = rand.Next(0, 8);
+            int y = rand.Next(0, 8);
+            for (int i = 0; i < total; ++i)
+            {
+                Rectangle r = new Rectangle
+                {
+                    Fill = new SolidColorBrush(Colors.Brown),
+                    Visibility = System.Windows.Visibility.Visible,
+                };
+                this.Entorno.Children.Add(r);
+                r.SetValue(Grid.ColumnProperty, x);
+                r.SetValue(Grid.RowProperty, y);
+                x = rand.Next(0, 8);
+                y = rand.Next(0, 8);
+            }
+
+        }
+
+        private void InitAgente()
+        {
+            this.aspiradora = new Agente(this.Entorno);
+        }
+
+        private void Button_MouseLeftButtonUp(object sender, EventArgs e)
+        {
+            this.aspiradora.MoverAgente();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < 64; i++)
+            {
+                this.aspiradora.MoverAgente();
+                //System.Threading.Thread.Sleep(500);
+            }
+
+        }
+    }
+}
